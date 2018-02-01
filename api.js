d63e30e10e69d49cfd910ce578a188c4de8c572c
Katie Poeltler <katiepoeltler@Katies-MacBook-Pro.local>
2018-02-01 11:32:59 -0600

    colorURL and api test

---
+++ api.js
@@ -0,0 +1,12 @@
+
+		var colorURL = "http://thecolorapi.com/id?hex=0047AB&format=html"
+
+
+		
+    $.ajax({
+      url: colorURL,
+      method: "GET"
+    }).then(function(response) {
+      var hex = $("<div>").text(response.hex.value)
+      
+    });
