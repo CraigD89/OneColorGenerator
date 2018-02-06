5ed988b117eb0d5171f7503ab78c857e6381233b
Katie Poeltler <katiepoeltler2018u.northwestern.edu>
2018-02-06 10:40:36 -0600

    added google api

--- api.js
+++ api.js
@@ -1,16 +1,14 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //Random hex generator
 
-  var hex = function () {
-    var letters = '0123456789ABCDEF';
-    var color = '';
+  var hex = function() {
+    var letters = "0123456789ABCDEF";
+    var color = "";
     for (var i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16) | 0];
-    };
-
+    }
 
     console.log("Random color is ", color);
-
 
     var queryURLBase = "http://www.thecolorapi.com/id?format=json&hex=" + color;
     console.log("url", queryURLBase);
@@ -18,7 +16,7 @@ $(document).ready(function () {
     $.ajax({
       url: queryURLBase,
       method: "GET"
-    }).then(function (response) {
+    }).then(function(response) {
       // $("#hexInfo").append(response.hex.value);
       // $("#hexInfo").append(response.name.value);
 
@@ -32,30 +30,54 @@ $(document).ready(function () {
       console.log("res", response);
       console.log("Hex is", response.hex.value);
       console.log("Color name is ", response.name.value);
+      var colorName = response.name.value;
+      console.log("colorName", colorName);
     });
+//start of chain to googleapi
+// 
+// var googleURL = "jenna's url";
+//       $.ajax({
+//         url: "googleURL + colorName +",
+//         method: "GET"
+//       }).then(function(results) {
+//         console.log("googleURL", results);
+//       });
+//------------------ after the ajax 'GET' comes back with results dig down and----------------- 
+//----------------find then create a for loop ----------
+
+    // for (var i = 0; i < results.googleapi.images.results.length; i++)){
+//   create new element that says $("<img>").w3lookup(src=bossresponse.images.results.result.clickurlform the yahoo response) look up syntax add a src and alt tag }
+//   append to empty div
+//-----------once we know what we need to dig down for we can jquery to add the images to the div or put this in
+//a for loop-----------------------------------------------------------------------------------------------
+
+//       $("#craigsnewdiv").text(results.new.drill.param);
+//       console.log("results.new.drll.param", body.new.drill.param);
+    // });
+    
     // $("body").css("background-color", hex());
     return color;
-
+    
   };
   $("body").css("background-color", "#" + hex());
 
-  //on click 
-  $("#generatorBtn").click(function () {
+  //on click
+  $("#generatorBtn").click(function() {
     hex();
     $("body").css("background-color", "#" + hex());
-
   });
 
-
-
-
-
-  $("#info").click(function () {
+  $("#info").click(function() {
     $("#toggle").toggle(".projectInfo");
   });
 
+$("#craig'scamerabtn").click(function(){
+  $("#togglemaybe").toggle(".projectInfo");
 
 });
+
+  });
+
 //------------------------------create a render fxn --------------------------
 // var renderButton = function () {
 
@@ -70,7 +92,6 @@ $(document).ready(function () {
 //       return color;
 
 //       // console.log("Random color is ", color);
-
 
 //       var queryURLBase = "http://www.thecolorapi.com/id?format=json&hex=" + hex;
 
@@ -96,14 +117,12 @@ $(document).ready(function () {
 
 // };
 
-
-//click event will change background color on each click 
+//click event will change background color on each click
 // var setRandomColor = function () {
 
 // $("body").css("background-color", hex());
 //   $("colorChange").click("setRandomColor()");
 // };
-
 
 //----------------------------------------------------------------------------------
 
@@ -135,12 +154,11 @@ $(document).ready(function () {
 //add shadow to button
 //if black turn elements to white
 
-
 //-----------------------PSEUDO CODE YAHOO API FEATURE---------------------
-//search for yahoo.dev tools 
+//search for yahoo.dev tools
 //parameter search: img
 // api "https://yboss.yahooapis.com/ysearch/images?q={keywords}[&param1=val1&param2=val2&etc]"
-//turn on filters 
+//turn on filters
 
 // var queryURLYahoo = "https://yboss.yahooapis.com/ysearch/images?q=dog+ show&format=json&img.dimensions=wallpaper]"
 
@@ -151,21 +169,18 @@ $(document).ready(function () {
 
 //   //<add to html link tag> target="_blank";
 
-// need url's from the response Json bossresponse.images.results.result.clickurl 
+// need url's from the response Json bossresponse.images.results.result.clickurl
 // //change xml to json
 // //&count to 10
-// build for loop index array 
+// build for loop index array
 // var yahooImg =[];
 
 // for (var i = 0; i < results.bossresponse.images.results.length; i++) {
 //   color += letters[Math.floor(Math.random() * 16) | 0];
 // };
 
-
 //--------------------Project Information Feature--------animation slider--------
 
 // function drawanimation () {
 
 // };
-
-
